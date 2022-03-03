0ecfaa01ec0757e410464a43f6fa569610e4c328
marcello <marcellopettenuzzo2@gmail.com>
2022-03-03 17:01:14 +0100

    header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,26 @@
+  <div class="header">
+    <div class="align_content_header">
+      <div class="logo_img">
+          <img src="{{ asset('images/dc-logo.png') }}" alt="">
+      </div>
+      <nav class="right_navigation">
+        <ul>
+          <li class="nav_menu selected">
+            <a href="#" >Characters</a>
+          </li>
+          <li class="nav_menu">
+            <a href="#" >Comics</a>
+          </li>
+          <li class="nav_menu">
+            <a href="#" >Movies</a>
+          </li>
+          <li class="nav_menu">
+            <a href="#" >Tv</a>
+          </li>       
+          <li class="nav_menu">
+            <a href="#" >Games</a>
+          </li>                                 
+        </ul>
+      </nav>
+    </div>
+</div>
