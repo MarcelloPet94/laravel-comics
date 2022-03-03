beab5c07c2b57404ffd96d2329df317d9b890a84
marcello <marcellopettenuzzo2@gmail.com>
2022-03-03 20:16:33 +0100

    thumb

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,10 +7,7 @@
         <link rel="stylesheet" href="{{asset('css/app.css')}}">        
     </head>
     <body>
-        
         @include('partials.header')
-        @dump($array_data)
         @include('product')
-        
     </body>
 </html>
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -4,12 +4,14 @@
             <div class="label"><span>current series</span></div>
         </div>
         <div class="thumb_container">
-            <div class="box_thumb">
-                    <div class="img_thumb">
-                        <img src="" alt="">
-                    </div>
-                <p>Nome</p>
-            </div>   
+            @foreach ($array_data as $i => $data)
+                <div class="box_thumb">
+                        <div class="img_thumb">
+                            <img src="{{$data['thumb']}}" alt="">
+                        </div>
+                    <p>{{$data['title']}}</p>
+                </div>                   
+            @endforeach
         </div>  
     </div>
 </div>
