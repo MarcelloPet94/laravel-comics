899325e5dbafdb512a0641395e3de0fb42587691
marcello <marcellopettenuzzo2@gmail.com>
2022-03-03 17:31:41 +0100

    bg-image ( errore )

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -24,3 +24,18 @@
       </nav>
     </div>
 </div>
+<div>
+    <div class="main">
+        <div class="hero_banner">
+            <div class="label"><span>current series</span></div>
+        </div>
+        <div class="thumb_container">
+            <div class="box_thumb">
+                    <div class="img_thumb">
+                        <img src="" alt="">
+                    </div>
+                <p>Nome</p>
+            </div>   
+        </div>  
+    </div>
+</div>
